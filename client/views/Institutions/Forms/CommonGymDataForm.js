a1d81229e16b6832be3c8552991be0fad01feeb6
DESKTOP-B89H4VQ\Alex <alexandr.biliaiev@gmail.com>
2017-07-12 20:03:33 +0200

    Working on GymDetails

---
+++ client/views/Institutions/Forms/CommonGymDataForm.js
@@ -0,0 +1,213 @@
+import React, {Component} from 'react';
+import {host} from "../../../global"
+import Spinner from "../../Components/Spinners/Spinner";
+import {saveFighter} from "../../../actions/UsersActions";
+
+import axios from "axios";
+import {connect} from "react-redux";
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker.css';
+
+
+import {Field, reduxForm} from 'redux-form';
+
+const inputIconStyle = {
+    width: '25px'
+}
+
+const socialMediaInputStyleStyle = {
+    zIndex: 0
+}
+
+const RenderDatePicker = props => {
+    return (
+        <div>
+            <DatePicker
+                {...props.input}
+                dateFormat="DD-MM-YYYY"
+                selected={props.input.value
+                ? moment(props.input.value)
+                : null}
+                className="form-control"/> {props.touched && props.error && <span>{props.error}</span>}
+        </div>
+    );
+};
+
+
+let CommonGymDataForm = props => {
+    const {handleSubmit, pristine, reset, submitting} = props;
+    return (
+
+        <form onSubmit={handleSubmit}>
+            <div className="card">
+                <div
+                    className="card-header"
+                    style={{
+                    backgroundColor: 'white'
+                }}>
+                    <i class="fa fa-id-card-o" aria-hidden="true"></i>
+                    Common
+                </div>
+                <div className="card-block">
+                    <div className="form-group row">
+                        <label className="col-md-3 form-control-label" htmlFor="text-input">Name</label>
+                        <div className="col-md-9">
+                            <Field
+                                name="name"
+                                component="input"
+                                type="text"
+                                className="form-control"
+                                placeholder="Gym name"/>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-md-3 form-control-label" htmlFor="text-input">Surname</label>
+                        <div className="col-md-9">
+                            <Field
+                                name="surname"
+                                component="input"
+                                className="form-control"
+                                type="text"
+                                placeholder="Surname"/>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-md-3 form-control-label" htmlFor="text-input">Nationality</label>
+                        <div className="col-md-9">
+                            <Field
+                                name="nationality"
+                                component="input"
+                                className="form-control"
+                                type="input"
+                                placeholder="Nationality"/>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-md-3 form-control-label" htmlFor="text-input">Birthdate</label>
+                        <div className="col-md-9">
+                            <Field name="birthdate" component={RenderDatePicker} type="input"/>
+                        </div>
+                    </div>
+
+                    <div className="form-group row">
+                        <label className="col-md-3 form-control-label" htmlFor="text-input">Gender</label>
+                        <div className="col-md-9">
+                            <Field name="gender" component="select" className="form-control">
+                                <option />
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                            </Field>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-md-3 form-control-label" htmlFor="text-input">Phone</label>
+                        <div className="col-md-9">
+                            <Field
+                                name="phone"
+                                component="input"
+                                className="form-control"
+                                type="text"
+                                placeholder="Phone"/>
+                        </div>
+                    </div>
+
+
+                    
+                </div>
+            </div>
+
+            <div className="card">
+                <div
+                    className="card-header"
+                    style={{
+                    backgroundColor: 'white'
+                }}>
+                    <i class="fa fa-facebook-square" aria-hidden="true"></i>
+                    Social media
+                </div>
+                <div className="card-block">
+                    <div className="form-group row">
+                        <div className="col-md-12">
+                            <div className="input-group">
+                                <span className="input-group-addon">
+                                    <i className="fa fa-facebook" style={inputIconStyle}></i>
+                                </span>
+                                <Field
+                                    name="facebook"
+                                    component="input"
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Facebook"
+                                    style={socialMediaInputStyleStyle}/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <div className="col-md-12">
+                            <div className="input-group">
+                                <span className="input-group-addon">
+                                    <i className="fa fa-instagram" style={inputIconStyle}></i>
+                                </span>
+                                <Field
+                                    name="instagram"
+                                    component="input"
+                                    className="form-control"
+                                    type="text"
+                                    placeholder="Instagram"
+                                    style={socialMediaInputStyleStyle}/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="form-group row">
+                        <div className="col-md-12">
+                            <div className="input-group">
+                                <span className="input-group-addon">
+                                    <i className="fa fa-twitter" style={inputIconStyle}></i>
+                                </span>
+                                <Field
+                                    name="twitter"
+                                    component="input"
+                                    className="form-control"
+                                    type="input"
+                                    placeholder="Twitter"
+                                    style={socialMediaInputStyleStyle}/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="form-group row">
+                        <div className="col-md-12">
+                            <div className="input-group">
+                                <span className="input-group-addon">
+                                    <i className="fa fa-vk" style={inputIconStyle}></i>
+                                </span>
+                                <Field
+                                    name="vk"
+                                    component="input"
+                                    className="form-control"
+                                    type="input"
+                                    placeholder="Vk"
+                                    style={socialMediaInputStyleStyle}/>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <button
+                type="button"
+                type="submit"
+                disabled={pristine || submitting}
+                className="btn btn-primary pull-right">Save</button>
+
+        </form>
+    );
+};
+
+export default reduxForm({
+    form: 'CommonGymDataForm', // a unique identifier for this form
+})(CommonGymDataForm);
