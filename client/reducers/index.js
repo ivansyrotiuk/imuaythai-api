81d87bfb493b9df78f091c03536904ea15539aad
Ivan Syrotiuk <waserdx@ukr.net>
2017-07-17 16:46:07 +0200

    remode singlecontesttype reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,7 +3,6 @@ import Gyms from "./GymsReducer"
 import Fighters from "./FightersReducer"
 import ContestTypes from "./ContestTypesReducer"
 import ContestRanges from "./ContestRangesReducer"
-import SingleContestRange from "./SingleContestRangeReducer"
 import Countries from "./CountriesReducer"
 import KhanLevels from "./KhanLevelsReducer"
 import { reducer as reduxFormReducer } from 'redux-form';
