9389fe542feae1ea9dfaf887b7097adb7b681704
Ivan Syrotiuk <waserdx@ukr.net>
2017-08-18 13:41:41 +0200

    fixed this fuckin git commit fail

--- api/Fights/FightersTossupper.cs
+++ api/Fights/FightersTossupper.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MuaythaiSportManagementSystemApi.Fights
 {
-    public class FightersTossupper
+    public class FightersTossupper: IFightersTossupper
     {
         public void Tossup(List<ApplicationUser> fighters, FightsTree tree)
         {
