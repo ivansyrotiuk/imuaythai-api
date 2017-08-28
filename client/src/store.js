b0491f4c392ff43bad1837336c7e86a5504f3295
Brejw <tomasz.walass@gmail.com>
2017-08-28 11:18:34 +0200

    turn off logger in production

--- client/src/store.js
+++ client/src/store.js
@@ -1,15 +1,23 @@
-import { applyMiddleware, createStore, compose } from "redux"
-import logger from "redux-logger"
-import thunk from "redux-thunk"
-import promise from "redux-promise-middleware"
-import reducer from "./reducers"
-import { loadState } from "./localStorage"
-import { setAuthToken } from "./axiosConfiguration"
+import { applyMiddleware, createStore, compose } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import promise from "redux-promise-middleware";
+import reducer from "./reducers";
+import { loadState } from "./localStorage";
+import { setAuthToken } from "./axiosConfiguration";
 const persistedState = loadState();
 
 if (persistedState != undefined && persistedState.Account != undefined) {
-  setAuthToken(persistedState.Account.authToken)
+  setAuthToken(persistedState.Account.authToken);
 }
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const middleware = [promise(), thunk, logger];
-export default createStore(reducer, persistedState, composeEnhancers(applyMiddleware(...middleware)));
+let middleware = [promise(), thunk];
+
+if (process.env.NODE_ENV !== "production") {
+  middleware = [...middleware, logger];
+}
+export default createStore(
+  reducer,
+  persistedState,
+  composeEnhancers(applyMiddleware(...middleware))
+);
