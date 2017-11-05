8e396c1e51d36b066bb23692c385ea6c27b70982
Tomasz Walas <tomasz.walass@gmail.com>
2017-11-05 17:48:31 +0100

    user details changes

--- client/src/views/Users/UserDetailsPage.js
+++ client/src/views/Users/UserDetailsPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Collapsible from "react-collapsible";
 import DocumentContainer from "../../containers/Users/UserDocumentContainer";
 import UserDetails from "./UserDetails";
+import UserFightDetails from "./UserFightDetails";
 import "../Styles/accord.css";
 class UserDetailsPage extends Component {
   render() {
@@ -9,36 +10,35 @@ class UserDetailsPage extends Component {
       <div className="animated fadeIn">
         <div className="row">
           <div className="col-12">
-
-
-              <Collapsible trigger="User details"
-                           triggerClassName="card-header"
-                           triggerOpenedClassName="card-header"
-                           contentInnerClassName="card-content"
-                           classParentString="card-override"
+            <Collapsible
+              trigger="User details"
+              triggerClassName="card-header"
+              triggerOpenedClassName="card-header"
+              contentInnerClassName="card-content"
+              classParentString="card-override"
               open={true}>
-                <div className="card-body">
-                  <UserDetails user={this.props.user} />
-                </div>
-              </Collapsible>
+              <div className="card-body">
+                <UserDetails user={this.props.user} />
+              </div>
+            </Collapsible>
 
-              <Collapsible trigger="Fights" triggerClassName="card-header"
-                           triggerOpenedClassName="card-header"
-                           contentInnerClassName="card-content"
-                           classParentString="card-override">
-                <p>
-                  An Accordion is different to a Collapsible in the sense that
-                  only one "tray" will be open at any one time.
-                </p>
-              </Collapsible>
+            <Collapsible
+              trigger="Fights"
+              triggerClassName="card-header"
+              triggerOpenedClassName="card-header"
+              contentInnerClassName="card-content"
+              classParentString="card-override">
+              <UserFightDetails />
+            </Collapsible>
 
-              <Collapsible trigger="Documents" triggerClassName="card-header"
-                           triggerOpenedClassName="card-header"
-                           contentInnerClassName="card-content"
-                           classParentString="card-override">
-                <DocumentContainer type="user" id={this.props.user.id} />
-              </Collapsible>
-
+            <Collapsible
+              trigger="Documents"
+              triggerClassName="card-header"
+              triggerOpenedClassName="card-header"
+              contentInnerClassName="card-content"
+              classParentString="card-override">
+              <DocumentContainer type="user" id={this.props.user.id} />
+            </Collapsible>
           </div>
         </div>
       </div>
