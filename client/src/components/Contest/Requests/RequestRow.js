a2b433391d4fafbc5a73505cf260bb1e9303f03a
Ivan Syrotiuk <ivan.syrotiuk@gmail.com>
2017-11-12 21:01:21 +0100

    Feactor of contest requests

---
+++ client/src/components/Contest/Requests/RequestRow.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+    CONTEST_REQUEST_PENDING,
+    CONTEST_REQUEST_ACCEPTED,
+    CONTEST_REQUEST_REJECTED
+} from '../../../common/contestRequestStatuses'
+
+const RequestRow = props => {
+    const {request} = props;
+
+    return (
+        <tr>
+            <td className="col-2">
+                {request.userName}
+            </td>
+            <td className="col-2">
+                {request.institutionName}
+            </td>
+            <td className="col-1">
+                {request.user && request.user.city}
+            </td>
+            <td className="col-2">
+                {request.user && request.user.countryName}
+            </td>
+            {request.contestCategoryId && <td>
+                {request.contestCategoryName}
+            </td>}
+            <td>
+                {request.status === CONTEST_REQUEST_PENDING && <span className="badge badge-warning">Pending</span>}
+                {request.status === CONTEST_REQUEST_ACCEPTED && <span className="badge badge-success">Accepted</span>}
+                {request.status === CONTEST_REQUEST_REJECTED && <span className="badge badge-danger">Rejected</span>}
+            </td>
+            <td>
+                {request.acceptedByUserName}
+            </td>
+            <td>
+                {props.children}
+            </td>
+        </tr>
+    );
+};
+
+RequestRow.defaultProps = {
+    request: {}
+};
+
+RequestRow.propTypes = {
+    request: PropTypes.obj
+};
+
+export default RequestRow;
--- client/src/views/Contest/ContestRequestsManager.js
+++ client/src/views/Contest/ContestRequestsManager.js
@@ -1,171 +1,88 @@
-import React, { Component } from 'react'
-import { TabContent, TabPane, Nav, NavItem, NavLink } from 'reactstrap';
-import { Route, Link } from 'react-router-dom';
+import React, {Component} from 'react'
+import {TabContent, TabPane, Nav, NavItem, NavLink} from 'reactstrap';
 import classnames from 'classnames';
-import AcceptButton from '../Components/Buttons/AcceptButton'
-import RejectButton from '../Components/Buttons/RejectButton'
-
-
-import { CONTEST_FIGHTER, CONTEST_JUDGE, CONTEST_DOCTOR } from '../../common/contestRoleTypes'
-import { CONTEST_REQUEST_PENDING, CONTEST_REQUEST_ACCEPTED, CONTEST_REQUEST_REJECTED } from '../../common/contestRequestStatuses'
+import {CONTEST_FIGHTER, CONTEST_JUDGE, CONTEST_DOCTOR} from '../../common/contestRoleTypes'
+import RequestsTable from "../../components/Contest/Requests/RequestsTable";
 
 class ContestRequestsManager extends Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.mapRequest = this.mapRequest.bind(this);
-    this.state = {
-      activeTab: '1'
-    };
-  }
-
-  toggle(tab) {
-    if (this.state.activeTab !== tab) {
-      this.setState({
-        activeTab: tab
-      });
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            activeTab: '1'
+        };
     }
-  }
 
-  mapRequest(request, i) {
-    const {acceptContestRequest, rejectContestRequest} = this.props;
+    toggle(tab) {
+        if (this.state.activeTab !== tab) {
+            this.setState({
+                activeTab: tab
+            });
+        }
+    }
 
-    return <tr key={ i }>
-             <td className="col-2">
-               { request.userName }
-             </td>
-             <td className="col-2">
-               { request.institutionName }
-             </td>
-             <td className="col-1">
-               { request.user.city }
-             </td>
-             <td className="col-2">
-               { request.user.countryName }
-             </td>
-             { request.contestCategoryId && <td>
-                                              { request.contestCategoryName }
-                                            </td> }
-             <td>
-               { request.status === CONTEST_REQUEST_PENDING && <span className="badge badge-warning">Pending</span> }
-               { request.status === CONTEST_REQUEST_ACCEPTED && <span className="badge badge-success">Accepted</span> }
-               { request.status === CONTEST_REQUEST_REJECTED && <span className="badge badge-danger">Rejected</span> }
-             </td>
-             <td>
-               { request.acceptedByUserName }
-             </td>
-             <td>
-               <RejectButton rejecting={ request.rejecting } click={ () => rejectContestRequest(request) } />
-               <AcceptButton accepting={ request.accepting } click={ () => acceptContestRequest(request) } />
-             </td>
-           </tr>
-  }
 
-  render() {
-    const {contest, requests, acceptContestRequest, rejectContestRequest} = this.props;
+    render() {
+        const {requests, acceptContestRequest, rejectContestRequest} = this.props;
 
-    const mappedFightersRequests = requests.filter(r => r.type === CONTEST_FIGHTER)
-      .map((request, i) => this.mapRequest(request, i, acceptContestRequest, rejectContestRequest))
+        const fightersRequests = requests.filter(r => r.type === CONTEST_FIGHTER);
+        const judgesRequests = requests.filter(r => r.type === CONTEST_JUDGE);
+        const doctorsRequests = requests.filter(r => r.type === CONTEST_DOCTOR);
 
-    const mappedJudgesRequests = requests.filter(r => r.type === CONTEST_JUDGE)
-      .map((request, i) => this.mapRequest(request, i, acceptContestRequest, rejectContestRequest))
+        const actions = {
+            accept: acceptContestRequest,
+            reject: rejectContestRequest
+        };
 
-    const mappedDoctorsRequests = requests.filter(r => r.type === CONTEST_DOCTOR)
-      .map((request, i) => this.mapRequest(request, i, acceptContestRequest, rejectContestRequest))
-
-    return (
-      <div>
-        <Nav tabs>
-          <NavItem>
-            <NavLink className={ classnames({
-                                   active: this.state.activeTab === '1'
-                                 }) } onClick={ () => {
-                                                                                                                                                                          this.toggle('1');
-                                                                                                                                                                        } }>
-              <i className="fa fa-user"></i> Fighters  <span className="badge badge-pill badge-primary"> { mappedFightersRequests.length }</span>
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink className={ classnames({
-                                   active: this.state.activeTab === '2'
-                                 }) } onClick={ () => {
-                                                                                                                                                                          this.toggle('2');
-                                                                                                                                                                        } }>
-              <i className="fa fa-gavel"></i> Judges <span className="badge badge-pill badge-success"> { mappedJudgesRequests.length }</span>
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink className={ classnames({
-                                   active: this.state.activeTab === '3'
-                                 }) } onClick={ () => {
-                                                                                                                                                                          this.toggle('3');
-                                                                                                                                                                        } }>
-              <i className="fa fa-user-md"></i> Doctors <span className="badge badge-pill badge-danger"> { mappedDoctorsRequests.length }</span>
-            </NavLink>
-          </NavItem>
-        </Nav>
-        <TabContent activeTab={ this.state.activeTab }>
-          <TabPane tabId="1">
-            <div className="h6">Requests:</div>
-            <table className="table table-hover mb-0 hidden-sm-down">
-              <thead>
-                <tr>
-                  <th className="col-2">Name</th>
-                  <th>Gym</th>
-                  <th>City</th>
-                  <th>Country</th>
-                  <th>Category</th>
-                  <th>Status</th>
-                  <th>Accepted by</th>
-                  <th className="text-center">Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                { mappedFightersRequests }
-              </tbody>
-            </table>
-          </TabPane>
-          <TabPane tabId="2">
-            <table className="table table-hover mb-0 hidden-sm-down">
-              <thead>
-                <tr>
-                  <th className="col-2">Name</th>
-                  <th>Gym</th>
-                  <th>City</th>
-                  <th>Country</th>
-                  <th>Status</th>
-                  <th>Accepted by</th>
-                  <th className="text-center">Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                { mappedJudgesRequests }
-              </tbody>
-            </table>
-          </TabPane>
-          <TabPane tabId="3">
-            <table className="table table-hover mb-0 hidden-sm-down">
-              <thead>
-                <tr>
-                  <th className="col-2">Name</th>
-                  <th>Gym</th>
-                  <th>City</th>
-                  <th>Country</th>
-                  <th>Status</th>
-                  <th>Accepted by</th>
-                  <th className="text-center">Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                { mappedDoctorsRequests }
-              </tbody>
-            </table>
-          </TabPane>
-        </TabContent>
-      </div>
-    )
-  }
+        return (
+            <div>
+                <Nav tabs>
+                    <NavItem>
+                        <NavLink className={classnames({
+                            active: this.state.activeTab === '1'
+                        })} onClick={() => {
+                            this.toggle('1');
+                        }}>
+                            <i className="fa fa-user"></i> Fighters <span
+                            className="badge badge-pill badge-primary"> {fightersRequests.length}</span>
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className={classnames({
+                            active: this.state.activeTab === '2'
+                        })} onClick={() => {
+                            this.toggle('2');
+                        }}>
+                            <i className="fa fa-gavel"></i> Judges <span
+                            className="badge badge-pill badge-success"> {judgesRequests.length}</span>
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className={classnames({
+                            active: this.state.activeTab === '3'
+                        })} onClick={() => {
+                            this.toggle('3');
+                        }}>
+                            <i className="fa fa-user-md"></i> Doctors <span
+                            className="badge badge-pill badge-danger"> {doctorsRequests.length}</span>
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+                <TabContent activeTab={this.state.activeTab}>
+                    <TabPane tabId="1">
+                        <div className="h6">Requests:</div>
+                        <RequestsTable requests={fightersRequests} actions={actions}/>
+                    </TabPane>
+                    <TabPane tabId="2">
+                        <RequestsTable requests={judgesRequests} actions={actions}/>
+                    </TabPane>
+                    <TabPane tabId="3">
+                        <RequestsTable requests={doctorsRequests} actions={actions}/>
+                    </TabPane>
+                </TabContent>
+            </div>
+        )
+    }
 }
 
 export default ContestRequestsManager
