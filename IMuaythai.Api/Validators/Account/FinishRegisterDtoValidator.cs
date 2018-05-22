e3f083f16cf2eb67fc47f88fd613a84c5d2ed7dc
Ivan Syrotiuk <ivan.syrotiuk@gmail.com>
2018-05-22 20:24:42 +0200

    Remove wrong validation

--- IMuaythai.Api/Validators/Account/FinishRegisterDtoValidator.cs
+++ IMuaythai.Api/Validators/Account/FinishRegisterDtoValidator.cs
@@ -7,10 +7,8 @@ namespace IMuaythai.Api.Validators.Account
     {
         public FinishRegisterDtoValidator()
         {
-            RuleFor(x => x.UserId).NotNull().WithMessage("UserId can not be null");
             RuleFor(x => x.CountryId).NotNull().GreaterThan(0).WithMessage("CountryId can not be null");
             RuleFor(x => x.FirstName).NotNull().WithMessage("FirstName can not be null");
-            RuleFor(x => x.GymName).NotNull().When(x => x.InstitutionId != null).WithMessage("GymName can not be null");
             RuleFor(x => x.Surname).NotNull().WithMessage("Surname can not be null");
             RuleFor(x => x.RoleId).NotNull().WithMessage("RoleId can not be null");
         }
