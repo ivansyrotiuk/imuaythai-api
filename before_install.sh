3ad0ee97969dbbba669491106e5ad5334262d581
Ivan Syrotiuk <jbt873@motorolasolutions.com>
2018-08-17 13:21:46 +0200

    Run before_install under sudo

--- before_install.sh
+++ before_install.sh
@@ -1,5 +1,5 @@
 wget -q https://packages.microsoft.com/config/ubuntu/14.04/packages-microsoft-prod.deb
-dpkg -i packages-microsoft-prod.deb
-apt-get install apt-transport-https
-apt-get update
-apt-get install dotnet-hosting-2.0.9
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install dotnet-hosting-2.0.9
