3439883d3fa3bf7598c66423c61c68d30650eb88
Ivan Syrotiuk <ivan.syrotiuk@gmail.com>
2018-10-05 19:36:50 +0200

    Clean up travis configuration

--- before_install.sh
+++
@@ -1,5 +0,0 @@
-wget -q https://packages.microsoft.com/config/ubuntu/14.04/packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-sudo apt-get install apt-transport-https
-sudo apt-get update
-sudo apt-get install dotnet-hosting-2.0.9
