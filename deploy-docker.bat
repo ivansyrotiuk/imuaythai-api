ed10654452a6339da8583b2ab2d49ce1afc2732b
Ivan Syrotiuk <ivan.syrotiuk@gmail.com>
2018-10-05 20:02:50 +0200

    Clean up travis configuration

--- deploy-docker.bat
+++
@@ -1,6 +0,0 @@
-dotnet publish -c Release
-copy ./Docker ./IMuaythai.Api/bin/release/netcoreapp2.0/publish
-docker build -t imuaythai-api ./IMuaythai.Api/bin/release/netcoreapp2.0/publish
-docker tag imuaythai-api imuaythai
-docker tag imuaythai-api:imuaythai  waserdx/imuaythai:imuaythai
-docker push waserdx/imuaythai:imuaythai
