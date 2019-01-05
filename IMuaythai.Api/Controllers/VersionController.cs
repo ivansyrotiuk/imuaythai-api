90c0b3608b30d711c6a9edacdf8c29978bcc4697
Ivan Syrotiuk <ivan.syrotiuk@gmail.com>
2019-01-05 22:36:50 +0100

    Added payment callback

--- IMuaythai.Api/Controllers/VersionController.cs
+++ IMuaythai.Api/Controllers/VersionController.cs
@@ -1,15 +1,54 @@
-﻿using System.Reflection;
+﻿using System;
+using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace IMuaythai.Api.Controllers
 {
     [Route("api/[controller]")]
     public class VersionController : Controller
     {
+        private readonly ILogger _logger;
+
+        public VersionController(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<VersionController>();
+        }
+
         public IActionResult Index()
         {
-            var version = Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.ApplicationVersion;
+            var version = Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application
+                .ApplicationVersion;
             return Ok(version);
+        }
+
+        [HttpGet]
+        [Route("pay")]
+        public IActionResult PaymentCallback()
+        {
+            using (StreamReader stream = new StreamReader(HttpContext.Request.Body))
+            {
+                string body = stream.ReadToEnd();
+                Console.WriteLine(body);
+                _logger.Log(LogLevel.Critical, body);
+            }
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("pay")]
+        public IActionResult PaymentCallbackPost()
+        {
+            using (StreamReader stream = new StreamReader(HttpContext.Request.Body))
+            {
+                string body = stream.ReadToEnd();
+                Console.WriteLine(body);
+                _logger.Log(LogLevel.Critical, body);
+            }
+
+            return Ok();
         }
     }
 }
