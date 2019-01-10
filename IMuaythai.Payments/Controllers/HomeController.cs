34e9fd0a22805032647b106c0f3193e3bd15039b
Ivan Syrotiuk <ivan.syrotiuk@gmail.com>
2019-01-10 22:50:29 +0100

    Added Payments
    # Conflicts:
    #	IMuaythai.sln

--- IMuaythai.Payments/Controllers/HomeController.cs
+++ IMuaythai.Payments/Controllers/HomeController.cs
@@ -1,81 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Diagnostics;
 using System.Linq;
-using System.Net;
-using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-using IMuaythai.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace IMuaythai.Api.Controllers
+namespace IMuaythai.Payments.Controllers
 {
-    [Route("api/[controller]")]
-    public class VersionController : Controller
+    public class HomeController : Controller
     {
         private readonly ILogger _logger;
-        private readonly IEmailSender _emailSender;
         private readonly IPayments24Client _payments24Client;
 
-        public VersionController(ILoggerFactory loggerFactory, IEmailSender emailSender, IPayments24Client payments24Client)
+        public HomeController(ILoggerFactory loggerFactory,  IPayments24Client payments24Client)
         {
-            _logger = loggerFactory.CreateLogger<VersionController>();
-            _emailSender = emailSender;
+            _logger = loggerFactory.CreateLogger<HomeController>();
             _payments24Client = payments24Client;
         }
 
         public IActionResult Index()
         {
-            var version = Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application
-                .ApplicationVersion;
-            return Ok(version);
-        }
-
-        [HttpGet]
-        [Route("pay")]
-        public async Task<IActionResult> PaymentCallback([FromQuery]PaymentStatus status)
-        {
-            try
-            {
-
-                await _payments24Client.Pay(new Dictionary<string, string>
-                {
-                    {"p24_merchant_id", status.p24_merchant_id.ToString()},
-                    {"p24_pos_id", status.p24_pos_id.ToString()},
-                    {"p24_session_id", status.p24_session_id},
-                    {"p24_amount", status.p24_amount.ToString()},
-                    {"p24_currency", status.p24_currency},
-                    {"p24_order_id", status.p24_order_id.ToString()},
-                    {"p24_sign", PaymentSigner.Sign(status.p24_session_id, status.p24_order_id, status.p24_amount, status.p24_currency, "b5c0e98687b0f43d")}
-                });
-
-
-                var s = Newtonsoft.Json.JsonConvert.SerializeObject(status);
-                await _emailSender.SendEmailAsync("waserdx@gmail.com", "test payment", s);
-                Console.WriteLine(s);
-                _logger.Log(LogLevel.Error, s);
-                return Ok(status);
-            }
-            catch (Exception ex)
-            {
-                var s = Newtonsoft.Json.JsonConvert.SerializeObject(status);
-
-                await _emailSender.SendEmailAsync("waserdx@gmail.com", "test payment", ex.ToString());
-                Console.WriteLine(ex);
-
-                throw;
-            }
+            Console.WriteLine(Request.QueryString.ToString());
+            return View();
         }
 
         [HttpPost]
-        [Route("pay")]
-        public async Task<IActionResult> PaymentCallbackPost([FromForm]PaymentStatus status)
+        public async Task<IActionResult> Post(PaymentStatus status)
         {
             try
             {
+
                 await _payments24Client.Pay(new Dictionary<string, string>
                 {
                     {"p24_merchant_id", status.p24_merchant_id.ToString()},
@@ -90,23 +47,18 @@ namespace IMuaythai.Api.Controllers
 
                 var s = Newtonsoft.Json.JsonConvert.SerializeObject(status);
                 Console.WriteLine(s);
-                await _emailSender.SendEmailAsync("waserdx@gmail.com", "test payment", s);
                 _logger.Log(LogLevel.Error, s);
-
-                return Ok(status);
             }
             catch (Exception ex)
             {
                 var s = Newtonsoft.Json.JsonConvert.SerializeObject(status);
 
-                await _emailSender.SendEmailAsync("waserdx@gmail.com", "test payment", ex.ToString());
                 Console.WriteLine(ex);
 
                 throw;
             }
+            return View();
         }
-
-
     }
 
     public class PaymentStatus
---
+++ IMuaythai.Payments/Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RestEase;
+
+namespace IMuaythai.Payments
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var paymentsClient = RestEase.RestClient.For<IPayments24Client>("https://sandbox.przelewy24.pl/");
+            services.AddSingleton<IPayments24Client>(paymentsClient);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+
+    public interface IPayments24Client
+    {
+        [Post("trnVerify")]
+        Task<Response<object>> Pay([Body(BodySerializationMethod.UrlEncoded)]Dictionary<string, string> body);
+    }
+}
